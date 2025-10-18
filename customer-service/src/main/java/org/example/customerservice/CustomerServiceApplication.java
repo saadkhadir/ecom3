6f8422601520689ba995086d0713e8757f25c323
saadkhadir <saad.khadir2-etu@etu.univh2c.ma>
2025-10-18 13:20:22 +0100

    first commit

---
+++ customer-service/src/main/java/org/example/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,36 @@
+package org.example.customerservice;
+
+import org.example.customerservice.entities.Customer;
+import org.example.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+
+    @Bean
+    CommandLineRunner CommandLineRunner(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(Customer.builder()
+                    .name("John Doe")
+                    .email("John@gmail.com")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("Saad")
+                    .email("saad@gmail.com")
+                    .build());
+            customerRepository.findAll().forEach(customer -> {
+                System.out.println(customer.getName());
+                System.out.println(customer.getEmail());
+            });
+        };
+
+}
+}
